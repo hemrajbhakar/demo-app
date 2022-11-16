662aec607eb1e958cb474d2f7f18cf307d484f2f
Hemraj Bhakar <hemrajbhakar123@gmail.com>
2022-11-16 15:30:22 +0530

    initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === "POST") {
+    let body = JSON.parse(req.body);
+    let mult = body.a * body.b;
+    let sum = parseInt(body.a) + parseInt(body.b);
+    if (body.calc_type === "sum") {
+      res.status(200).json({ result: sum });
+    } else {
+      res.status(200).json({ result: mult });
+    }
+  } else {
+    return null;
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState({ a: null, b: null, calc_type: null });
+  const [result, setResult] = useState(null);
+  const handleInputChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    setData((prevState) => ({ ...prevState, [name]: value }));
+    console.log(data);
+  };
+  console.log(data);
+  const xyz = async (e) => {
+    e.preventDefault();
+    const bodyFile = {
+      method: "POST",
+      headers: { "Content-Type": "aplication/json" },
+      body: JSON.stringify(data),
+    };
+    const response = await fetch("/api/hello", bodyFile).then((t) => t.json());
+    setResult(response.result);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,60 +34,30 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <form onSubmit={xyz}>
+          <input type="number" name="a" required onChange={handleInputChange} />
+          <input type="number" name="b" required onChange={handleInputChange} />
+          <input
+            type="radio"
+            id="sum"
+            name="calc_type"
+            onChange={handleInputChange}
+            value="sum"
+          />
+          <label htmlFor="sum">Sum</label>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <input
+            type="radio"
+            id="mult"
+            name="calc_type"
+            onChange={handleInputChange}
+            value="mult"
+          />
+          <label htmlFor="mult">Mult</label>
+          <button type="submit">Submit</button>
+        </form>
+        {result}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
